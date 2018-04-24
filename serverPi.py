b24d22b118fe48eccabc2b16596c186bb8730bce
Sam Bair <sbair15@vt.edu>
2018-04-23 22:29:44 -0400

    Added serverPi.py

---
+++ serverPi.py
@@ -0,0 +1,10 @@
+#Will be used for the server code 
+
+#Section to listen for a signal from signal pi
+
+#Loop to listen for rfid reads
+# Might need a timer from threading
+
+#After Loop send message to database with appropriate times
+
+#Continue loop until there are no more races
