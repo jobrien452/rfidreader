e486e456c2a2f572fbdd65cdd2aca55988229f85
Sam Bair <sbair15@vt.edu>
2018-04-26 21:45:57 -0400

    added some arg parse stuff

--- serverPi.py
+++ serverPi.py
@@ -1,3 +1,12 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+#Takes in the arguments and assigns proper values that correspond to the flags, defaults are included
+parser.add_argument("-t", "--timer_ip", dest = "serverIP", default = "192.168.0.8", help = "Timer IP")
+parser.add_argument("-d", "--debug", dest = "serverPort", default = "0", help = "Debug Mode")
+
+args = parser.parse_args()
 #Will be used for the server code 
 
 #Section to listen for a signal from signal pi
