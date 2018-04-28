67c3f7d5216a7bc7cd86ad0b9d1fb436ab7be393
Matt DelVecchio <matdd96@vt.edu>
2018-04-28 17:12:00 -0400

    Added socket communication

--- starter.py
+++ starter.py
@@ -4,12 +4,30 @@ import wave
 import numpy as np
 import time
 import sys
+import socket
+import argparse
 
 CHUNK = 512
 FORMAT = pyaudio.paInt16 #paInt8
 CHANNELS = 1
 RATE = 44100 #sample rate
 RECORD_SECONDS = 5
+
+parser = argparse.ArgumentParser()
+
+#Takes in the arguments and assigns proper values that correspond to the flags, defaults are included
+parser.add_argument("-s", "--server_ip", dest = "serverIP", default = "192.168.0.8", help = "Server IP")
+parser.add_argument("-p", "--server_port", dest = "serverPort", default = "8888", help = "Server Port")
+parser.add_argument("-z", "--socket_size", dest = "socketSize", default = "4096", help = "Socket Size")
+
+args = parser.parse_args()
+
+
+
+#This assigns the arguments to the corresponding 
+IP = args.serverIP
+PORT = int(args.serverPort)
+SIZE = int(args.socketSize)
 
 p = pyaudio.PyAudio()
 
@@ -51,3 +69,38 @@ stream.stop_stream()
 stream.close()
 p.terminate()
 
+#####
+#This section creates the socket, connection, and sends the data
+#####
+#create the socket and connect
+try:
+    #Create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except (socket.error, msg):
+    print("Failed to create socket. Error Code: ", str(msg[0]), ", Error message: ", msg[1])
+    sys.exit()
+        
+print("[Checkpoint] Socket Created")
+        
+try:
+    #This connects to the server
+    s.connect((IP, PORT))
+    print("[Checkpoint] Connecting to " + IP + " on port " + repr(PORT))
+
+except:
+    print("Failed to connect to the server")
+    sys.exit()
+
+start = "start"
+s.sendall(start)
+print("Race started")
+
+dataBack = s.recv(SIZE)
+
+responseBack = bytes.decode(dataBack)
+if "Done" in responseBack:
+    print("Race has finished")
+    s.close();
+    
+else:
+    print("Unknown response")
